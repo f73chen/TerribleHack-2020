250e9b3bd7c98ea6c0e09744b96bc71cb873b287
g3chen <g3chen@oicr.on.ca>
2020-05-16 16:54:34 -0400

    Added Manager

---
+++ TerribleElevator/TerribleElevator/Manager.cpp
@@ -0,0 +1,7 @@
+#include "Manager.hpp"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <string>
+using namespace std;
---
+++ TerribleElevator/TerribleElevator/Manager.hpp
@@ -0,0 +1,13 @@
+#ifndef MANAGER_HPP
+#define MANAGER_HPP
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <string>
+using namespace std;
+
+class Manager{
+
+};
+#endif
