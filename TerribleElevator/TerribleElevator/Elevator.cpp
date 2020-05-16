13a72d0d3258bd638194859c59fab303a454fdf1
g3chen <g3chen@oicr.on.ca>
2020-05-16 15:25:34 -0400

    Elevator cpp and hpp rough draft

--- TerribleElevator/TerribleElevator/Elevator.cpp
+++ TerribleElevator/TerribleElevator/Elevator.cpp
@@ -1,2 +1,109 @@
+#include "Elevator.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <string>
+using namespace std;
 
+// PURPOSE: checks the position, direction, buttons pressed, and nextStop to determine whether it moves at 
+// INPUT: nnextStop given by the manager, tells where the next stop along the path someone pressed a button
+void Elevator::move() {
+	if (validMove()) {
+		speed = nextSpeed(); // calculates how fast to travel next
+		// NOTE: directX should use the speed information to know how fast to animate movement
+		floor += direction; // if direction positive then go up, else stay put or go down
+	}
+	else {
+		cout << "Error: invalid move.\nFloor: " << floor << ", direction: " << direction << ", speed: " << speed << endl;
+	}
+}
 
+// PURPOSE: checks whether the car can move to the next space specified
+bool Elevator::validMove() {
+	// if manager tells the car to not move
+	if (!canMove) { return false; }
+
+	// if already at the top floor and going up
+	if (floor == FLOORS && direction > 0) { return false; }
+
+	// if already at the bottom floor and going down
+	if (floor == 0 && direction < 0) { return false; }
+}
+
+// PURPOSE: checks current floor, direction, stopping distance and next floor to determine what speed it should travel at
+// NOTE: when arriving at the destination floor, speed should be less than accel (either 0 or accel/2)
+int Elevator::nextSpeed() {
+	// if move is invalid, it should be stopped
+	if (!validMove()) { return 0; }
+	int absDist = abs(floor - nextStop);		// absolute distance between current position and target
+	int accel = MAX_SPEED / stoppingDistance;	// how much it can accelerate and decelerate at each floor
+
+	// if remaining distance is more than stopping distance
+	if (absDist > stoppingDistance) {
+		speed += accel;	// speed up by one floor's worth of acceleration
+		if (speed >= MAX_SPEED) { // if over the speed limit set it back to the limit
+			speed = MAX_SPEED;
+		}
+	}
+	else { // else within range of stopping, might have to slow down
+		if (speed == 0) { // if at rest, must accelerate a bit to move
+			if (absDist > stoppingDistance/2) { // if have plenty of room
+				speed += accel; // then acceleration; plan to decelerate at the next floor
+			}
+			else { // if too close to properly accelerate, do partial
+				speed += accel / 2;
+			}
+		}
+		else if (speed == MAX_SPEED) { // if starting at max speed, then must slow down
+			speed -= accel;
+		}
+		else { // if speed is not 0 but not max either
+			if (speed - (absDist - 2) * accel <= 0) { //if have plenty of room to keep accelerating
+				speed += accel;
+			}
+			else { //if too close, must slow down
+				speed -= accel;
+			}
+		}
+	}
+}
+
+// PURPOSE: add an "additional" number of people into the elevator
+// OUTPUT: int is the number of people that couldn't make it in
+int Elevator::enter(int additional) {
+	if (numPeople + additional <= CAPACITY) { // everyone can fit
+		numPeople += additional;
+		return 0;
+	}
+	else { // some people are left behind on that floor
+		additional += (numPeople - CAPACITY);
+		numPeople = CAPACITY;
+		return additional;
+	}
+}
+
+// PURPOSE: remove a "less" number of people from the elevator
+void Elevator::leave(int left) {
+	if (numPeople < left) {
+		cout << "Error: more people left than are in the elevator.\nFloor: " << floor << ", left: " << left << endl;
+		return;
+	}
+	else {
+		numPeople -= left;
+	}
+}
+
+// PURPOSE: add a list of buttons that the newly added people will press
+void Elevator::pressButton(vector<int> floorNums) {
+	for (int i = 0; i < floorNums.size(); i++) { // iterates through the list of buttons pressed
+		buttonsPressed[floorNums[i]] = 1; // set status to pressed
+	}
+	buttonsPressed[floor] = 0; // can't press the button for the floor you're already on
+}
+
+// PURPOSE: arrived at a certain floor, have to clear info about button
+void Elevator::arrive() {
+	speed = 0; //resets travel speed
+	buttonsPressed[floor] = 0; // removes current floor from list of buttons
+}
--- TerribleElevator/TerribleElevator/Elevator.h
+++ TerribleElevator/TerribleElevator/Elevator.h
@@ -1,25 +1,62 @@
-#pragma once
+#ifndef ELEVATOR_HPP
+#define ELEVATOR_HPP
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <array>
+#include <string>
 using namespace std;
 
 class Elevator {
 private:
 	int floor;		// which floor it's on
-	int direction;	// -1 down, 0 stationary, 1 up
+	int speed;		// speed of travel
+	int direction;	// direction of travel: -1 for down, 1 for up, 0 for stopped
 	bool canMove;	// whether manager knows the door is closed and elevator can move again
-	vector<bool> buttonsPressed;	// vector of buttons, size is number of floors, 1 is pressed 0 is not pressed
-	int nextStop;	// sent by manager: either the next button pressed or the next floor with someone waiting
+	vector<bool> buttonsPressed;	// @@@@@@@@vector of buttons, size is number of floors, 1 is pressed 0 is not pressed
+	int nextStop;	// sent by manager: either the next button pressed or the next floor with someone waiting, or the bottom or top floor
 	int FLOORS;		// total number of floors
 	int CAPACITY;	// max capacity of the carriage
 	int numPeople;	// number of people currently in the elevator
+	int stoppingDistance;	// number of floors it takes for the elevator to go from full speed to stopped, if don't have this much room then will travel at 1/D speed
+	int MAX_SPEED;	// maximum speed the car can travel at
 public:
-	// constructors
-	Elevator() :floor(1), direction(0), canMove(false), buttonsPressed({false}), nextStop(1), FLOORS(1), CAPACITY(0), numPeople(0) {}
-	Elevator(int nfloor, int ndirection, bool ncanMove, vector<bool> nbuttonsPressed, int nnextStop, int nFLOORS, int nCAPACITY, int nnumPeople) :
-		floor(nfloor), direction(ndirection), canMove(ncanMove), buttonsPressed(nbuttonsPressed), nextStop(nnextStop), FLOORS(nFLOORS), CAPACITY(nCAPACITY), numPeople(nnumPeople) {}
+	// default constructor
+	Elevator() :floor(1), speed(0), direction(0), canMove(false), buttonsPressed({ false }), nextStop(1), FLOORS(1), CAPACITY(0), numPeople(0), stoppingDistance(1), MAX_SPEED(0) {}
+	
+	// constructor with parameters
+	Elevator(int nfloor, int nspeed, int ndirection, bool ncanMove, vector<bool> nbuttonsPressed, 
+		int nnextStop, int nFLOORS, int nCAPACITY, int nnumPeople, 
+		int nstoppingDistance, int nMAX_SPEED) :
+		floor(nfloor), speed(nspeed), direction(ndirection), canMove(ncanMove), buttonsPressed(nbuttonsPressed), 
+		nextStop(nnextStop), FLOORS(nFLOORS), CAPACITY(nCAPACITY), numPeople(nnumPeople), 
+		stoppingDistance(nstoppingDistance), MAX_SPEED(nMAX_SPEED) {}
 
+	// returns whether adding more people would not overload it
+	bool withinCapacity(int additional) { return numPeople + additional <= CAPACITY; }
 
+	// PURPOSE: checks the position, direction, buttons pressed, and nextStop to determine whether it moves at 
+	// INPUT: nnextStop given by the manager, tells where the next stop along the path someone pressed a button
+	void move();
+
+	// PURPOSE: checks whether the car can move to the next space specified
+	bool validMove();
+
+	// PURPOSE: checks current floor, direction, stopping distance and next floor to determine what speed it should travel at
+	// NOTE: when arriving at the destination floor, speed should be less than accel (either 0 or accel/2)
+	int nextSpeed();
+
+	// PURPOSE: add an "additional" number of people into the elevator
+	// OUTPUT: int is the number of people that couldn't make it in
+	int enter(int additional);
+
+	// PURPOSE: remove a "less" number of people from the elevator
+	void leave(int left);
+
+	// PURPOSE: add a list of buttons that the newly added people will press
+	void pressButton(vector<int> floorNums);
+
+	// PURPOSE: arrived at a certain floor, have to clear info about button
+	void arrive();
 };
+#endif
