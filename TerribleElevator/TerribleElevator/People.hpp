6b0827b264c7228942bfa90f19af8904e1c97f11
candaceng <candacewyng@gmail.com>
2020-05-16 12:59:07 -0400

    People object .hpp and .cpp files

---
+++ TerribleElevator/TerribleElevator/People.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <array>
+using namespace std;
+
+class People {
+private:
+	int curFloor;           // current floor
+    int goalFloor;          // destination floor
+    int walkingSpeed;       // rate at which they walk
+    int frustrationLevel;   // current frustration level
+    int frustrationCap;     // frustration capacity 
+public:
+	// constructors
+	People() {}
+    // methods
+    void randomize();       // randomizes properties
+    
+
+};
