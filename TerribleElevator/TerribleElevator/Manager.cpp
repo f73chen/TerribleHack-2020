b8ad9d27c8014f3883ffd9a53fe92bfee901c886
g3chen <g3chen@oicr.on.ca>
2020-05-16 18:19:04 -0400

    Fixed some syntax errors, added functions to Manager

--- TerribleElevator/TerribleElevator/Manager.cpp
+++ TerribleElevator/TerribleElevator/Manager.cpp
@@ -1,7 +1,31 @@
 #include "Manager.hpp"
+#include "Elevator.h"
+#include "Floor.hpp"
+#include "People.hpp"
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <array>
 #include <string>
 using namespace std;
+
+// initialize the vector of floors
+void Manager::initFloors() {
+	for (int i = 0; i < FLOORS; i++) {
+		floorList.push_back(new Floor(i + 1));
+	}
+}
+
+// initialize the vector of people
+void Manager::initPeople() {
+	for (int i = 0; i < PEOPLE; i++) {
+		peopleList.push_back(new People());
+	}
+}
+
+// initialize the vector of elevators
+void Manager::initElevators() {
+	for (int i = 0; i < ELEVATORS; i++) {
+		elevatorList.push_back(new Elevator());
+	}
+}
